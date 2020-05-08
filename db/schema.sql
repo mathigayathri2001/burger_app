ddb657d4ffba45e7d6d0764801d9b0256979d5b1
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-05-08 19:08:10 -0400

    create and insert the values in the database

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Drop, create and use the database
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+-- create the table 
+CREATE TABLE burgers
+(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devored BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+
+--  use the database
+ USE burger_db;
+
+-- INSER values in to the tables
+ INSERT INTO burgers (burger_name) values ('mac chicken burger');
+ INSERT INTO burgers (burger_name) values ('Ham burger');
+ INSERT INTO burgers (burger_name) values ('Happy Meal');
