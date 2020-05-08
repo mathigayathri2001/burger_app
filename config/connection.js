68d9e3c4989929c81b37e404ae7d3dfb3307095f
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-05-08 19:25:50 -0400

    created connection to the database

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2')
+ 
+let defaultConfig;
+
+if (process.env.JAWSDB_URL) {
+    defaultConfig = process.env.JAWSDB_URL
+}
+else {
+    defaultConfig = {
+        host: 'localhost',
+        user: 'root',
+        password: 'Gay141981$',
+        database: 'burger_db'
+    }
+}
+
+
+let connection = mysql.createConnection(defaultConfig)
+
+connection.promise()
+
+module.exports = connection
