5527be5d955bd67e39d897477a0e3fa734b9a708
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-05-09 20:54:51 -0400

    route functionality created

--- controller/burger_controller.js
+++ controller/burger_controller.js
@@ -11,10 +11,41 @@ router.get("/", async function (req, res) {
     const data = await Burger.findAll();
     res.render("index", { burgers: data });
   });
+// api route
+  router.get('/api/burgers', async function (req, res) {
+    try {
+      const burger = await Burger.findAll()
+      res.status(200).json({ data: burger })
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
 
+  router.post('/api/burgers', async function (req, res) {
+    try {
+      const addBurger = new Burger(req.body)
+      await addBurger.save()
+      res.status(201).json(addBurger)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
 
+  router.put('/api/burgers/:id', async (req, res) => {
 
+    const id = req.params.id
+    const burgerName = req.body.burger_name
+    const devoured = req.body.devoured
 
+    let updatedBurger = new Burger({ burgerName: burgerName, isDevoured: devoured, id })
+    try {
+        await updatedBurger.save()
+        res.status(200).json(updatedBurger)
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 
